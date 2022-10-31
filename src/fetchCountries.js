4e4210fa222dfd864f599105edbd8eeb4d5b5014
UsernameViktoriia <tori-boss@ukr.net>
2022-10-31 19:37:48 +0200

    виконано дз

---
+++ src/fetchCountries.js
@@ -0,0 +1,8 @@
+export default function fetchCountries(name) {
+  return fetch(`https://restcountries.com/v3.1/name/${name}`).then(response => {
+    if (response.ok) {
+      return response.json();
+    }
+    throw new Error(response.statusText);
+  });
+}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,69 @@
 import './css/styles.css';
+import Notiflix from 'notiflix';
+import debounce from 'lodash.debounce';
+import fetchCountries from './fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
+const input = document.querySelector('#search-box');
+const ulEl = document.querySelector('.country-list');
+const infoEl = document.querySelector('.country-info');
+
+input.addEventListener(
+  'input',
+  debounce(e => {
+    infoEl.innerHTML = '';
+    ulEl.innerHTML = '';
+    fetchCountries(e.target.value.trim())
+      .then(data => {
+        if (data.length === 1) {
+          appendMarkupInfo(data[0]);
+        } else if (data.length <= 10 && data.length >= 2) {
+          appendMarkupCountries(data);
+        } else {
+          Notiflix.Notify.info(
+            'Too many matches found. Please enter a more specific name.'
+          );
+        }
+      })
+      .catch(data =>
+        Notiflix.Notify.failure('Oops, there is no country with that name.')
+      );
+  }, DEBOUNCE_DELAY)
+);
+
+function appendMarkupInfo({
+  name: { official },
+  capital,
+  population,
+  flags: { svg },
+  languages,
+}) {
+  const markup = `
+        <div class="wrapper">
+        <img src = "${svg}" alt = "flag" width =40>
+        <p>${official}</p>
+        </div>
+        <div class = "desc">
+        <p>Capital: <span>${capital}</span></p>
+        <p>Population: <span>${population}</span></p>
+        <p>Languages: <span>${Object.values(languages)}</span></p>
+        </div>
+        
+        `;
+
+  infoEl.insertAdjacentHTML('beforeend', markup);
+}
+function appendMarkupCountries(data) {
+  const markup = data
+    .map(
+      ({ name: { official }, flags: { svg } }) =>
+        `
+        <li class='countries-item'>
+        <img src = "${svg}" alt = "flag" width = 40>
+        <p class='countries-title'>${official}</p>
+        </li>
+        `
+    )
+    .join('');
+  ulEl.insertAdjacentHTML('beforeend', markup);
+}
